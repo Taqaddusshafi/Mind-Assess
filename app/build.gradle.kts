05c861571a1b855e902fe88227a4ef63355413f2
taqaddusshafi <taqaddusshafi9086@gmail.com>
2025-05-22 10:36:26 +0530

    second push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.campusdisha.mindassess"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.campusdisha.mindassess"
